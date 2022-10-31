a820646da52088ddb39891585dff9b7a6aadea1a
Alexandre Schaffner <alexandre.schaffner@icloud.com>
2022-10-31 17:03:08 +0100

    [UPDATE/REMOVE] download filename; remove tar dependency

--- src/app.ts
+++ src/app.ts
@@ -10,21 +10,23 @@ import fs, { Stats } from 'fs'
 import { NetworkInterfaceInfo, networkInterfaces } from 'os'
 import path from 'path'
 import qrcode from 'qrcode-terminal'
-import tar from 'tar'
+import { spawn } from 'child_process'
 
 // ─────────────────────────────────────────────────────────────────────────────
 
 dotenv.config()
 
-async function main (): Promise<void> {
-  // ─── Error Handling ──────────────────────────────────────────────────
+function createTarball (dirPath: string): void {
+  console.log('Creating the tarball...')
+  const tarballName: string = (dirPath.split('/').pop() as string) + '.tar.gz'
+  const child = spawn('tar', ['-C', dirPath, '-czf', tarballName, '.'])
 
-  if (process.argv.length !== 3) throw new Error('Invalid arguments')
+  child.on('close', (data) => {
+    createQRCode()
+  })
+}
 
-  // ─── Init ────────────────────────────────────────────────────────────
-
-  const app: Application = express()
-
+function createQRCode (): void {
   // ─── Retrieve IP Address ─────────────────────────────────────────────
 
   const ip: string | undefined = Object.values(networkInterfaces())
@@ -36,27 +38,44 @@ async function main (): Promise<void> {
     ?.address
   if (ip === undefined) throw new Error('Cannot retrieve the local IP of this machine.')
   const address: string = 'http://' + ip + ':' + String(process.env.PORT)
-  const filePath: string = process.argv[2][0] === '/' ? process.argv[2] : path.resolve() + '/' + process.argv[2]
+
+  // ─── Generate The QR Code ────────────────────────────────────────────
+
+  qrcode.generate(address)
+}
+
+async function main (av: string[]): Promise<void> {
+  // ─── Error Handling ──────────────────────────────────────────────────
+
+  if (av.length !== 3) throw new Error('Invalid arguments')
+
+  // ─── Init ────────────────────────────────────────────────────────────
+
+  const app: Application = express()
+  let filePath: string = process.argv[2][0] === '/' ? process.argv[2] : path.resolve() + '/' + process.argv[2]
+  if (filePath[filePath.length - 1] === '/') filePath = filePath.slice(0, -1)
+  const fileName: string = filePath.split('/').pop() as string
   const stats: Stats = await fs.promises.stat(filePath)
 
   if (stats.isDirectory()) {
-    console.log('Creating the tarball...')
-    await tar.create({ gzip: true, file: 'directory_archive.tar.gz' }, [filePath])
+    createTarball(filePath)
+  } else {
+    createQRCode()
   }
-
-  // ─── Create The QR Code ──────────────────────────────────────────────
-
-  qrcode.generate(address)
 
   // ─── Route For Serving The File ──────────────────────────────────────
 
   app.get('/', async (req: Request, res: Response): Promise<void> => {
     if (stats.isDirectory()) {
-      res.download(path.resolve() + '/' + 'directory_archive.tar.gz')
+      res.download(path.resolve() + '/' + fileName + '.tar.gz')
       return
     }
-    console.log('Sending ' + filePath + '...')
+    console.log('Sending ' + fileName + '...')
     res.download(filePath)
+  })
+
+  app.get('/hello', (req: Request, res: Response) => {
+    res.send('Hello World !')
   })
 
   // ─── Start Server ────────────────────────────────────────────────────
@@ -75,8 +94,8 @@ async function main (): Promise<void> {
     server.close(async () => {
       debug('HTTP server closed.')
       try {
-        await fs.promises.access(path.resolve() + '/' + 'directory_archive.tar.gz')
-        await fs.promises.unlink(path.resolve() + '/' + 'directory_archive.tar.gz')
+        await fs.promises.access(path.resolve() + '/' + fileName + '.tar.gz')
+        await fs.promises.unlink(path.resolve() + '/' + fileName + '.tar.gz')
         process.exit(0)
       } catch (err: unknown) {
         process.exit(0)
@@ -89,7 +108,7 @@ async function main (): Promise<void> {
 // ─── Call To Main ────────────────────────────────────────────────────────────
 
 try {
-  main()
+  main(process.argv)
 } catch (err: unknown) {
   console.error(err)
 }
